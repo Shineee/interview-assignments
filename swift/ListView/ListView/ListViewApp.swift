eeb8b9d73d590e941ba55e315143fe4c728f32ef
wangyang <wyhdgx@163.com>
2022-03-04 23:59:57 +0800

    init commit

---
+++ swift/ListView/ListView/ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewApp.swift
+//  ListView
+//
+//  Created by admin on 2022/3/4.
+//
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
